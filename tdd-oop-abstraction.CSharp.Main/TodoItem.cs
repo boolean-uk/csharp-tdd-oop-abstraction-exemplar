a991fd881cea2d6994787636af36d6cea945f1b9
uerbzr <nigel.sibbert@gmail.com>
2023-09-13 21:40:07 +0100

    completed core

--- tdd-oop-abstraction.CSharp.Main/TodoItem.cs
+++ tdd-oop-abstraction.CSharp.Main/TodoItem.cs
@@ -7,23 +7,25 @@ using System.Threading.Tasks;
 namespace tdd_oop_abstraction.CSharp.Main
 {
     public class TodoItem {
-        
-        public string title;
-        public string detail;
-        public string status;
+        private string _status;
+        private string _detail;
+        private string _title;
+        private DateTime _created;
+        public TodoItem()
+        {
+            _created = DateTime.Now;
+        }
+        public string Title { get => _title; set => _title = value; }
+        public string Detail { get => _detail; set => _detail = value; }
+        public string Status { get => _status; set => _status = value; }
 
-        public TodoItem(String title, String detail, String status) {
-            this.title = title;
-            this.detail = detail;
-            this.status = status;
+        public TodoItem(string title, string detail, string status) 
+        {
+            this._title = title;
+            this._detail = detail;
+            this._status = status;
         }
 
-        public void setStatus(String status) {
-            this.status = status;
-        }
-
-        public String getStatus() {
-            return this.status;
-        }
+      
     }
 }
